8acd9182c1409f484d3f05169a6c190e52e6b51a
Gaby <gaby@gaby.com>
2024-09-20 11:11:45 -0300

    DOCS( Server.js ): Agrego info de las funcionaliades

--- Server.js
+++ Server.js
@@ -3,6 +3,9 @@ const PORT = 3000;
 const USER = 'Gabriel';
 
 const escucha = () => ({
+<<<<<<< Updated upstream
   // Se crear una función anónima y se informa en la consola
+=======
+>>>>>>> Stashed changes
   console.log(`Hola ${USER}! Escuchando en el puerto ${PORT}`);
 })
